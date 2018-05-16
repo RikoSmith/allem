fe06ca47319e3eb01d18bd8377800a234656fff9
KazutoSmith <raimbek.riko@gmail.com>
2018-05-16 09:18:51 +0600

    test added

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function (req, res) {
   res.render('error')
 })
 
-router.get('/rikosmith', function (req, res) {
+router.get('/test', function (req, res) {
   res.render('index')
 })
 
