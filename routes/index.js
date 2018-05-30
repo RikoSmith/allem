9ddd92c3da9831432e8f5e98a6e4314fcd2dd002
Riko <raimbek.riko@gmail.com>
2018-05-30 09:13:22 +0600

    added mongo connection db

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express')
 var router = express.Router()
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://admin:Zxc159Zxc159@ds139690.mlab.com:39690/allemdb';
+const dbName = 'allemdb';
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -21,5 +26,28 @@ router.get('/en', function (req, res) {
 router.get('/kz', function (req, res) {
   res.render('index')
 })
+
+router.get('/db', function (req, res) {
+  var testdb = "null";
+
+  MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+    const collection = db.collection('lang');
+
+    collection.find({}).toArray(function(err, docs) {
+      assert.equal(err, null);
+
+      res.send(docs);
+
+    });
+
+    client.close();
+  });
+})
+
+
 
 module.exports = router
