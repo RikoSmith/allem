9740dfea2256c6416c6124da9b34ce427f7397c8
Riko <raimbek.riko@gmail.com>
2018-06-12 10:34:34 +0600

    member page added

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
 
 
 
@@ -60,5 +61,31 @@ router.get('/test', function (req, res) {
 router.get('/map', function (req, res) {
   res.render('sb-admin/map_view')
 })
+
+
+router.get('/member', function (req, res) {
+  res.render('sb-admin/member')
+})
+
+router.get('/member/:member_id', function (req, res) {
+
+  MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+
+    const db = client.db(dbName);
+    const collection = db.collection('members');
+    var data = collection.findOne({"_id": new ObjectId(req.params.member_id)}, function(err, doc) {
+      if (err) throw err;
+      console.log(doc);
+      res.render('sb-admin/member', {member: doc});
+      client.close();
+    });
+
+  });
+
+})
+
+
+
 
 module.exports = router
