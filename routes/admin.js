ccc3ba628fc861b6115e97d88f49217cb04b29cf
Riko <raimbek.riko@gmail.com>
2018-06-14 12:09:16 +0600

    holidays added

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const dates =   require('../lib/dates');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
@@ -9,7 +10,54 @@ var ObjectId = require('mongodb').ObjectID;
 const url = 'mongodb://admin:Zxc159Zxc159@ds139690.mlab.com:39690/allemdb';
 const dbName = 'allemdb';
 
+function updateStatus(req, res, next){
+  MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
 
+    const db = client.db(dbName);
+    const collection = db.collection('members');
+    var data = collection.find({}).toArray(function(err, members) {
+      if (err) throw err;
+      //console.log(result);
+      var now = new Date();
+      for(var i = 0; i < members.length; i++){
+        var newValues = { $set: {} };
+
+        if(members[i].status_end_date){
+          console.log("end date has");
+          if(dates.compare(new Date(members[i].status_end_date), now) >= 0){
+            if(members[i].status !== "На работе") newValues.$set.status = "На работе";
+          }else{
+            newValues.$set.status = null;
+          }
+        }
+
+        if(members[i].holiday_start && members[i].holiday_end){
+          if(dates.inRange(now, new Date(members[i].holiday_start), new Date(members[i].holiday_end))){
+            if(members[i].status !== "В отпуске") newValues.$set.status = "В отпуске";
+          }else{
+            if(members[i].status_end_date){
+
+            }else{
+              if(newValues.$set.status !== "На работе")newValues.$set.status = "На работе";
+            }
+          }
+        }
+
+        if(newValues.$set.status){
+          console.log(members[i].lastname + " " + members[i].name);
+          collection.updateOne({"_id": new ObjectId(members[i]._id)}, newValues, function(err, response) {
+            if (err) throw err;
+            console.log("1 document updated  |" + response);
+          });
+        }
+
+      }
+      client.close();
+    });
+  });
+  next();
+}
 
 function checkSignIn(req, res, next){
    if(req.session.logged){
@@ -29,7 +77,7 @@ router.get('/', function (req, res) {
   res.render('sb-admin/index');
 })
 
-router.get('/members', function (req, res) {
+router.get('/members', updateStatus, function (req, res) {
   MongoClient.connect(url, function(err, client) {
     assert.equal(null, err);
 
@@ -78,7 +126,7 @@ router.get('/map', function (req, res) {
 })
 
 
-router.get('/member/:member_id', function (req, res) {
+router.get('/member/:member_id', updateStatus, function (req, res) {
 
   MongoClient.connect(url, function(err, client) {
     assert.equal(null, err);
@@ -97,8 +145,8 @@ router.get('/member/:member_id', function (req, res) {
 })
 
 
-router.post('/editMember', function (req, res) {
-  //console.log(req.body);
+router.post('/editMember', function (req, res, next) {
+  console.log(req.body);
   MongoClient.connect(url, function(err, client) {
     assert.equal(null, err);
 
@@ -107,27 +155,70 @@ router.post('/editMember', function (req, res) {
     const db = client.db(dbName);
     const collection = db.collection('members');
     var data = collection.findOne({"_id": new ObjectId(req.body.member_id)}, function(err, doc) {
-      if (err) {
-        throw err;
-        res.send("Ошибка с соединением с БД");
-      }
+      if (err) throw err;
 
       if(req.body.name) newValues.$set.name = req.body.name;
       if(req.body.lastname) newValues.$set.lastname = req.body.lastname;
       if(req.body.middlename) newValues.$set.middlename = req.body.middlename;
       if(req.body.position) newValues.$set.position = req.body.position;
       if(req.body.department) newValues.$set.department = req.body.department;
+
       if(req.body.start_date && req.body.end_date){
-        if(req.body.start_date < req.body.end_date){
+        var start = new Date(req.body.start_date);
+        var end = new Date(req.body.end_date);
+
+
+        if(dates.compare(start, end) > 0){
+
           newValues.$set.start_date = req.body.start_date;
           newValues.$set.end_date = req.body.end_date;
         }else{
           res.status(400);
           newValues = {};
-          res.send("Ошибка! Даты введены неправильно. Отмена всех изменении");
+          res.send("Ошибка! Даты срока сотрудничества введены неправильно. Отмена всех изменении");
         }
       }
-      //console.log(newValues);
+
+      if(req.body.status){
+
+        if(req.body.status == "На работе"){
+          newValues.$set.status = req.body.status;
+          newValues.$set.status_end_date = '';
+        }else{
+          var now = new Date();
+          var status_end = new Date(req.body.status_end_date);
+
+
+          if( dates.compare(now, status_end) <= 0 ){
+                newValues.$set.status = req.body.status;
+                newValues.$set.status_end_date = req.body.status_end_date;
+          }else {
+            res.status(400);
+            newValues = {};
+            res.send("Ошибка! Даты срока статуса введены неправильно. Отмена всех изменении");
+          }
+        }
+
+      }
+
+      if(req.body.holiday_start && req.body.holiday_end){
+        var h_start = new Date(req.body.holiday_start);
+        var h_end = new Date(req.body.holiday_end);
+        console.log("Start: " + h_start);
+        console.log("End: " + h_end);
+
+        if(dates.compare(h_start, h_end) < 0){
+          newValues.$set.holiday_start = req.body.holiday_start;
+          newValues.$set.holiday_end = req.body.holiday_end;
+        }else{
+          res.status(400);
+          newValues = {};
+          res.send("Ошибка! Даты срока отпуска введены неправильно. Отмена всех изменении");
+        }
+      }
+
+
+      console.log(newValues);
       if(newValues.$set){
         collection.updateOne({"_id": new ObjectId(req.body.member_id)}, newValues, function(err, response) {
           if (err) throw err;
@@ -139,7 +230,6 @@ router.post('/editMember', function (req, res) {
 
     });
   });
-
 
 })
 
