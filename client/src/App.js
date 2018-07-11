4e0e5dd393b2a4171d4ae6c76a8fd4016b1e4486
Riko <raimbek.riko@gmail.com>
2018-07-11 09:10:35 +0600

    some

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
             <Route exact path="/" component={PageLanding} />
             <Route exact path="/en" render={() => <PageLanding lang="en" />} />
             <Route exact path="/kz" render={() => <PageLanding lang="kz" />} />
+            <Route exact path="/ru" render={() => <PageLanding lang="ru" />} />
             <Route exact path="/admin" component={Admin} />
             <Route exact path="/login" component={PageLogin} />
             <Route component={PageNotFound} />
