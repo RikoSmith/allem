e0a69aa9f5326aa6ebef9356075661e76cea5cf2
Raiymbek Mustazhapov <r.mustazhapov@kazdream.kz>
2020-09-30 16:13:42 +0300

    url tweaks

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 const keys = {
-  MONGO_URI: 'mongodb://admin:Zxc159Zxc159@ds139690.mlab.com:39690/allemdb',
+  MONGO_URI: 'mongodb://admin:Zxc159Zxc159@ds139690.mlab.com:39690/allemdb?keepAlive=true&poolSize=30&autoReconnect=true&socketTimeoutMS=360000&connectTimeoutMS=360000',
   SESSION_SECRET: "AJFN7 58DK7 F9GOR 7854R",
   JWT_KEY: "45c49asd4f94h84tn94h5fg6h49?asd46&asdadad"
 }
